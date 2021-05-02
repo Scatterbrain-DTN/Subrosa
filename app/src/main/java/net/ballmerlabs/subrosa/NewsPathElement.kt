95278f86a1ba93e0dd51b25e1cc8b2957c8c0bb6
Alex Ballmer <gnu3ra@riseup.net>
2021-05-01 20:00:08 -0400

    add news hierarchy view on statusbar

---
+++ app/src/main/java/net/ballmerlabs/subrosa/NewsPathElement.kt
@@ -0,0 +1,86 @@
+package net.ballmerlabs.subrosa
+
+import android.content.Context
+import android.graphics.*
+import android.util.AttributeSet
+import android.view.View
+import kotlin.math.max
+
+class NewsPathElement(context: Context, attrs: AttributeSet) : View(context, attrs) {
+    var text: String
+    val cornerMargin: Float
+    private val xpad = max(paddingLeft + paddingRight.toFloat(), 64.toFloat())
+    private val ypad = max(paddingTop + paddingBottom.toFloat(), 64.toFloat())
+    private val textx = (width - xpad) / 2.0.toFloat()
+    private val texty = (height - ypad) / 2.0.toFloat()
+    private val textBounds = Rect()
+    private val textPaint = Paint().apply {
+        color = Color.GRAY
+        textAlign = Paint.Align.CENTER
+        textSize = 60f
+    }
+    
+    private val rectPaint = Paint().apply { 
+        color = Color.BLACK
+        style = Paint.Style.FILL
+    }
+
+    private val leftDoinkPath = Path()
+    
+    private val rightDoinkPath = Path()
+    
+    init {
+        context.theme.obtainStyledAttributes(
+                attrs,
+                R.styleable.NewsPathElement,
+                0,
+                0
+        ).apply {
+            try {
+                text = getString(R.styleable.NewsPathElement_text)?: "fmef"
+                textPaint.color = getColor(R.styleable.NewsPathElement_textColor, Color.GRAY)
+                rectPaint.color = getColor(R.styleable.NewsPathElement_backgroundColor, Color.BLACK)
+                cornerMargin = getDimension(R.styleable.NewsPathElement_edgeRadius, 128F)
+            } finally {
+                recycle()
+            }
+        }
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        textPaint.getTextBounds(text, 0, text.length, textBounds)
+        val w = textBounds.width() + xpad.toInt() + 256
+        val h = textBounds.height() + ypad.toInt()
+        setMeasuredDimension(
+                MeasureSpec.makeMeasureSpec(w, MeasureSpec.UNSPECIFIED),
+                MeasureSpec.makeMeasureSpec(h, MeasureSpec.EXACTLY)
+        )
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        leftDoinkPath.apply {
+            moveTo(cornerMargin, measuredHeight.toFloat())
+            lineTo(0F, measuredHeight.toFloat())
+            lineTo(cornerMargin, measuredHeight/2F)
+            lineTo(0F, 0F)
+            lineTo(cornerMargin, 0F)
+            close()
+        }
+
+        rightDoinkPath.apply {
+            moveTo(measuredWidth-cornerMargin, measuredHeight.toFloat())
+            lineTo(measuredWidth.toFloat(), measuredHeight/2F)
+            lineTo(measuredWidth - cornerMargin, 0F)
+            close()
+        }
+        canvas.apply {
+            val x = width.toFloat()/2
+            val y = (height.toFloat()/2.0.toFloat()) - (textPaint.descent() + textPaint.ascent())/2.0.toFloat()
+            drawRect(cornerMargin, 0.toFloat(), width - cornerMargin, height.toFloat(), rectPaint)
+            drawPath(leftDoinkPath, rectPaint)
+            drawPath(rightDoinkPath, rectPaint)
+            drawText(text, x, y, textPaint)
+        }
+    }
+}
---
+++ app/src/main/java/net/ballmerlabs/subrosa/NewsPathFlow.kt
@@ -0,0 +1,60 @@
+package net.ballmerlabs.subrosa
+
+
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.updateLayoutParams
+
+class NewsPathFlow(context: Context, private val attributeSet: AttributeSet) : ConstraintLayout(context, attributeSet) {
+    private val elements = ArrayList<NewsPathElement>()
+    var spacing: Int = 32
+
+
+    private fun createElement(name: String): NewsPathElement {
+        val path = NewsPathElement(context, attributeSet)
+        path.text = name
+        path.id = View.generateViewId()
+        addView(path)
+        return path
+    }
+
+    private fun setParams(path: NewsPathElement, prev: Int? = null) {
+        path.updateLayoutParams<LayoutParams> {
+            if (prev != null) {
+                startToEnd = prev
+                marginStart = (path.cornerMargin.toInt() * -1) + spacing
+            } else {
+                startToStart =  LayoutParams.PARENT_ID
+            }
+            topToTop = LayoutParams.PARENT_ID
+            bottomToBottom = LayoutParams.PARENT_ID
+        }
+    }  
+
+    fun setPaths(names: Array<String>) {
+        var prev = id
+        for (i in names.indices) {
+            val path = createElement(names[i])
+            setParams(path, if (i > 1) prev else null)
+            elements.add(path)
+            prev = path.id
+        }
+    }
+    
+    fun addPath(name: String) {
+        val path = createElement(name)
+        setParams(path, if(elements.size > 1) elements[elements.size-1].id else null)
+        elements.add(path)
+    }
+
+    fun removePath() {
+        if (elements.size > 0) {
+            val path = elements[elements.size - 1]
+            removeView(path)
+            elements.removeAt(elements.size-1)
+        }
+    }
+}
