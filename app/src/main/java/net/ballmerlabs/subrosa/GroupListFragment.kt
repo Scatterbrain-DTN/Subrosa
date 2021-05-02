80621c274510ddb10f278e83830d3eb7ec5d6b0f
Alex Ballmer <gnu3ra@riseup.net>
2021-05-01 20:36:28 -0400

    rename FirstFragment to GroupListFragment

--- app/src/main/java/net/ballmerlabs/subrosa/GroupListFragment.kt
+++ app/src/main/java/net/ballmerlabs/subrosa/GroupListFragment.kt
@@ -5,20 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.fragment.findNavController
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class GroupListFragment : Fragment() {
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.fragment_group_list, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
