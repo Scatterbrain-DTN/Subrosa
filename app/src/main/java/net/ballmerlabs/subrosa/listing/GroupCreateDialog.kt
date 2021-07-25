445a2cefc0fa421c0760643b91af5c111582f459
Alex Ballmer <gnu3ra@riseup.net>
2021-07-25 03:29:17 -0400

    set dialog softInputMode to adjustreize

--- app/src/main/java/net/ballmerlabs/subrosa/listing/GroupCreateDialog.kt
+++ app/src/main/java/net/ballmerlabs/subrosa/listing/GroupCreateDialog.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import android.widget.Toast
 import androidx.fragment.app.DialogFragment
 import androidx.navigation.findNavController
