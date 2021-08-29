3bf5e59a84c53c67efb1c1e7cb2fa8b1712302b1
Alex Ballmer <gnu3ra@riseup.net>
2021-08-29 13:40:11 -0400

    empty settings fragment

---
+++ app/src/main/java/net/ballmerlabs/subrosa/prefs/MainSettings.kt
@@ -0,0 +1,15 @@
+package net.ballmerlabs.subrosa.prefs
+
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+import dagger.hilt.android.AndroidEntryPoint
+import net.ballmerlabs.subrosa.R
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class MainSettings @Inject constructor() : PreferenceFragmentCompat() {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey)
+    }
+}
